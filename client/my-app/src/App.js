3f184736871694344debb01f403020134a9d0457
Thu Anh Ma <tam.thuma@gmail.com>
2025-07-22 18:07:36 +0200

    connect client and server

--- client/my-app/src/App.js
+++ client/my-app/src/App.js
@@ -1,7 +1,29 @@
 import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
-  return <div className="App"></div>;
+  const [listOfPosts, setListsOfPosts] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/cards").then((response) => {
+      setListsOfPosts(response.data);
+    });
+  }, []);
+
+  return (
+    <div className="App">
+      {listOfPosts.map((value, key) => {
+        return (
+          <div className="card">
+            {" "}
+            <div className="front"> {value.frontText} </div>
+            <div className="back"> {value.backText} </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default App;
